deed7ed7c01aa67ed950cf639bb6f55cfd941068
sratemo <sratemo@gmail.com>
2023-11-27 13:10:39 -0800

    user changes

--- backend/src/graphql/resolvers/User.ts
+++ backend/src/graphql/resolvers/User.ts
@@ -1,65 +1,72 @@
-import { getUserData } from '../../lib/jwt';
+// Lib
+import { getUserData } from '../../lib/jwt'
 
+// Interfaces
 import {
- IUser,
- ICreateUserInput,
- IModels,
- ILoginInput,
- IAuthPayLoad
-} from '../../types';
+  IUser,
+  ICreateUserInput,
+  IModels,
+  ILoginInput,
+  IAuthPayload
+} from '../../types'
 
-
-
-import { doLogin, getUserBy } from '../../lib/auth';
+// Utils
+import { doLogin, getUserBy } from '../../lib/auth'
 
 export default {
- Query: {
-  getUsers: (
-   _: any, args: any,
-   { models }: { models: IModels }
-  ): IUser[] => models.User.findAll(),
-  getUserData: async (
-   _: any,
-   { at }: { at: string },
-   { models }: { models: IModels }
-  ): Promise<any> => {
-   // Get current connected User 
-   const connectedUser = await getUserData(at);
+  Query: {
+    getUsers: (
+      _: any,
+      args: any,
+      { models }: { models: IModels }
+    ): IUser[] => models.User.findAll(),
+    getUserData: async (
+      _: any,
+      { at }: { at: string },
+      { models }: { models: IModels }
+    ): Promise<any> => {
+      // Current connected user
+      const connectedUser = await getUserData(at)
 
-   if (connectedUser) {
-    // validate if the user is still valid 
-    const user = await getUserBy({
-     id: connectedUser.id,
-     email: connectedUser.email,
-     privilege: connectedUser.privilege,
-     active: connectedUser.active
-    },
-     models
-    )
-    if (user) {
-     return connectedUser;
+      if (connectedUser) {
+        // Validating if the user is still valid
+        const user = await getUserBy(
+          {
+            id: connectedUser.id,
+            email: connectedUser.email,
+            privilege: connectedUser.privilege,
+            active: connectedUser.active
+          },
+          models
+        )
+
+        if (user) {
+          return {
+            ...connectedUser
+          }
+        }
+      }
+
+      return {
+        id: '',
+        username: '',
+        password: '',
+        email: '',
+        privilege: '',
+        active: false
+      }
     }
-   }
-
-   return {
-    id: '',
-    username: '',
-    password: '',
-    email: '',
-    privilege: '',
-    active: false
-   }
+  },
+  Mutation: {
+    createUser: (
+      _: any,
+      { input }: { input: ICreateUserInput },
+      { models }: { models: IModels }
+    ): IUser => models.User.create({ ...input }),
+    login: (
+      _: any,
+      { input }: { input: ILoginInput },
+      { models }: { models: IModels }
+    ): Promise<IAuthPayload> => doLogin(input.email, input.password, models)
   }
- },
-
- Mutation: {
-  createUser: (_: any,
-   { input }: { input: ICreateUserInput },
-   { models }: { models: IModels }): IUser => models.create({ ...input }),
-
-  login: (_: any,
-   { input }: { input: ILoginInput },
-   { models }: { models: IModels }
-  ): Promise<IAuthPayLoad> => doLogin(input.email, input.password, models)
- }
-};
+}
